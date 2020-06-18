955c410b5fe0141942a3a55a4941e4ed9bbe2fc5
NataliaD <natalia123doroz@gmail.com>
2020-06-18 15:37:15 +0200

    Create class ToDo, Person, Test and methods and objects. This in constructors

--- src/Person.java
+++ src/Person.java
@@ -1,21 +1,3 @@
-//Napisz aplikację z listą do zarządzania zadaniami. Każde zadanie ma nazwę, opis, priorytet oraz osobę odpowiedzialną
-// za wykonanie (osoba reprezentowana jest jako osobna klasa). W klasie powinny znajdować się konstruktory:
-//
-//pozwalający zainicjować wszystkie pola,
-//pozwalający zainicjować wszystkie pola oprócz osoby,
-//pozwalający zainicjować wszystkie pola oprócz osoby i priorytetu
-//W zadaniu wykorzystaj dwa poznane zastosowania słowa kluczowego this.
-//
-//Zdefiniuj 3 metody:
-//
-//highPriority() zwraca true, jeśli priorytet zadania jest wysoki
-//mediumPriority() zwraca true, jeśli priorytet zadania jest normalny
-//lowPriority() zwraca true, jeśli priorytet zadania jest niski
-//Zadanie ma priorytet wysoki, jeśli do pola priorytet przypisana jest wartość dodatnia, normalny oznacza wartość 0, a niski wartość ujemną.
-//
-//Utwórz kilka przykładowych obiektów reprezentujących zadania i przetestuj stworzone metody wyświetlając wartości ich priorytetów.
-//
-//*Jeżeli konstruktory zapisałeś samodzielnie, to je usuń i wygeneruj ponownie korzystając ze środowiska (Alt + Insert / ⌘ + N)
 public class Person {
     String firstName;
 
